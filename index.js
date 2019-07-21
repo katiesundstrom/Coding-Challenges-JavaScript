5787f87fd92f4bf737a854ae89e3c3dae38b313b
Katie Sundstrom <ksiegel@wellesley.edu>
2019-07-21 10:44:01 -0400

    adds prompt for superbalanced binary tree

--- index.js
+++ index.js
@@ -355,3 +355,16 @@ function getProductsOfAllIntsExceptAtIndex(intArray) {
 
     return products;
   }
+
+  // ---------------------------------------------------------------
+
+/*
+ Write a function to see if a binary tree ↴ is "superbalanced" (a new tree property we just made up).
+
+A tree is "superbalanced" if the difference between the depths of any two leaf nodes ↴ is no greater than one.
+
+Big O Time: O()
+Big O Space: O()
+*/
+
+

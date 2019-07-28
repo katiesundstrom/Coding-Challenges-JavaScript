c5182855be92adb732351d4bfde35d9d3b4013bc
Katie Sundstrom <ksiegel@wellesley.edu>
2019-07-28 14:19:54 -0400

    adds prompt for dictionaryWordFinder

--- index.js
+++ index.js
@@ -473,7 +473,7 @@ An array of words has been rotated. Write a function to return the index of the 
   'othellolagkage',
 ];
 
-Big O Time: O()
+Big O Time: O(nlogn)
 Big O Space: O()
 */
 
@@ -491,10 +491,32 @@ function findRotationPoint(words) {
     return findRotationPoint(words.slice(0, middleIndex))
   }
 
-  if (middleWord >= firstWord) {
+  if (middleWord > firstWord) {
     return findRotationPoint(words.slice(middleIndex))
   }
 
   return false;
 }
 
+    // ---------------------------------------------------------------
+
+/*
+
+Given an alphabetical array of dictionary entries and a word to search for, find that word's definition (if it exists). A dictionary entry is just a string where the word's name appears first, followed by - [definition].
+
+const dictionary = [
+  'a - Used when mentioning someone or something for the first time in a text or conversation',
+  'and - Used to connect words of the same part of speech, clauses, or sentences, that are to be taken jointly',
+  'be - Exist',
+  'in - Expressing the situation of something that is or appears to be enclosed or surrounded by something else',
+  'of - Expressing the relationship between a part and a whole',
+  'that - Used to identify a specific person or thing observed or heard by the speaker',
+  'the - Denoting one or more people or things already mentioned or assumed to be common knowledge',
+  'to - Expressing motion in the direction of (a particular location)'
+];
+definitionOf('be', dictionary); // should return 'Exist'
+definitionOf('that', dictionary); // should return 'Used to identify a specific person or thing observed or heard by the speaker'
+definitionOf('to', dictionary); // should return 'Expressing motion in the direction of (a particular location)'
+definitionOf('wizbing', dictionary); // should return undefined
+
+*/

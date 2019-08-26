342dfb36fa0b4397206dbeac763a4b02ff41833a
Katie Sundstrom <ksiegel@wellesley.edu>
2019-08-26 09:10:20 -0400

    adds prompt for doesPathExist

--- index.js
+++ index.js
@@ -963,3 +963,28 @@ function maximumIncreasingSubsequence(array) {
   }
   return Math.max(...lengths);
 }
+
+
+/* ---------------------------------------------------------------
+
+Prompt: Write a function that determines if a path exists between two vertices of a directed graph.
+
+The graph will be represented as an object, each of whose keys represents a vertex of the graph and whose value represents all vertices that can be reached from the aforementioned key.
+
+In the example below, there is a connection from vertex a to vertex b and a connection from vertex b to vertices c and d but not a connection from vertex b to vertex a.
+
+{
+  a: ['b'],
+  b: ['c', 'd'],
+  c: ['d']
+}
+
+Big O:
+Time: O()
+Space: O()
+
+*/
+
+function doesPathExist(graph, node1, node2, seen = new Set()) {
+  
+}
